02b5731ddc7c974cb1c20a1ce48e1409d731cea2
Mukesh Gupta <eshhjd@gmail.com>
2023-05-01 20:24:20 +0530

    initial commit

---
+++ src/test/java/com/springProjects/examples/SpringGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springProjects.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
